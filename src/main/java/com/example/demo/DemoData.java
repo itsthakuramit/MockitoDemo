e6e43771db4a5bc28685fe4af6cd3b2cbce80568
Koel <koel.chowdhury@stcakroute.in>
2021-05-17 17:16:43 +0530

    commit

---
+++ src/main/java/com/example/demo/DemoData.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoData 
+{
+	List<String> carList = new ArrayList<String>();
+	
+	public DemoData()
+	{
+		carList.add("Swift");
+		carList.add("Ertiga");
+	}
+	
+	
+	public List<String> getCars()
+	{
+		return carList;
+		
+	}
+	
+	public List<String> addCar(String car)
+	{
+		carList.add(car);
+		
+		return carList;
+	}
+
+}
